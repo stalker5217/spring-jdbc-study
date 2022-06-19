f3ae190f9aff739d7e95a197256ff399daf55379
songmk <stalker5217@gmail.com>
2022-06-19 10:42:12 +0900

    first commit

---
+++ src/main/java/com/example/springjdbcstudy/SpringJdbcStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjdbcstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springjdbcstudy/SpringJdbcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjdbcstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
